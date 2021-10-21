0dbe8440cc58f35f082ff98826814b96bca6e3ca
knch1014 <knch1014@gmail.com>
2021-10-21 13:23:58 +0900

    더 깔끔하게

--- movies/views.py
+++ movies/views.py
@@ -11,16 +11,26 @@ class ActorListView(View):
     
 
     def get(self, request):
-       
-        results = [{'name' : f'{actor.last_name}{actor.first_name}', 'titles' : [{'title' : movie.title} for movie in actor.movie_actor.all()]} for actor in Actor.objects.all()]
+        results = [
+            {
+                'name'   : f'{actor.last_name}{actor.first_name}', 
+                'titles' : [{'title' : movie.title} for movie in actor.movie_actor.all()]
+            } for actor in Actor.objects.all()
+        ]
         
         return JsonResponse({'result' : results}, status=200)
+
 class MovieListView(View):
     
 
 
     def get(self, request):
-       
-        results = [{"title" : movie.title, "running_time" : movie.running_time, "name" : [{'first_name' : actor.first_name} for actor in movie.actors.all()]} for movie in Movie.objects.all()]
+        results = [
+            {
+                "title"        : movie.title, 
+                "running_time" : movie.running_time, 
+                "name" : [{'first_name' : actor.first_name} for actor in movie.actors.all()]
+            } for movie in Movie.objects.all()
+        ]
        
         return JsonResponse({'result' : results}, status=200)
--- owners/views.py
+++ owners/views.py
@@ -19,8 +19,14 @@ class OwnerListView(View):
         return JsonResponse({'Message':'Created'}, status=201)
    
     def get(self, request):
-       
-        results = [{"name" : owner.name, "email" : owner.email, "age" : owner.age, "dogs" : [{"name" : dog.name, "age" : dog.age} for dog in owner.owners.all()]} for owner in Owner.objects.all()]
+        results = [
+            {
+                "name"  : owner.name, 
+                "email" : owner.email, 
+                "age"   : owner.age,
+                "dogs"  : [{"name" : dog.name, "age" : dog.age} for dog in owner.owners.all()]
+            } for owner in Owner.objects.all()
+        ]
 
         return JsonResponse({'result':results}, status=200)
 
@@ -28,17 +34,24 @@ class DogListView(View):
 
     def post(self, request):
         data = json.loads(request.body)
+
         Dog.objects.create(
             name = data["name"],
             owner_id = Owner.objects.get(id= data["owner_id"]),
             age = data["age"],
         )
+
         return JsonResponse({'Message':'Created'}, status=201)
 
 
     def get(self, request):
-       
-        results = [{'name' : dog.name, 'age' : dog.age, 'owner' :[{'name' : dog.owner.name}]} for dog in Dog.objects.all()]
+        results = [
+            {
+                'name' : dog.name, 
+                'age' : dog.age, 
+                'owner' :[{'name' : dog.owner.name}]
+            } for dog in Dog.objects.all()
+        ]
 
         return JsonResponse({'result':results}, status=200)
             
